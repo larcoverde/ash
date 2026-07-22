02b631e5318ec26f9c3d203d52e2cd827fe83b07
Lucas Arcoverde <larcoverde@proton.me>
2026-07-22 20:56:10 -0300

    Make print welcome banner.

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
+#include <stdio.h>
+
+void
+print_banner()
+{
+printf("    Welcome to\n\n");
+printf("           _ \n");
+printf("  __ _ ___| |__ \n");
+printf(" / _` / __| '_ \\ \n");
+printf("| (_| \\__ \\ | | | \n");
+printf(" \\__,_|___/_| |_| \n\n");
+printf("    - a shell -\n\n");
+printf("type 'help' to get started\n");
+}
+
 int
 main(int argc, char* argv[])
 {
+    print_banner();
     return 0;
 }
